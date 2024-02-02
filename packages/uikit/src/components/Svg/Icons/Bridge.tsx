1a4f357bca16fcd315aa334f28bdbad8e525b18f
towerdevswap <akmalhanif672@gmail.com>
2024-02-02 16:50:13 +0700

    fix scan

--- packages/uikit/src/components/Svg/Icons/Bridge.tsx
+++ packages/uikit/src/components/Svg/Icons/Bridge.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<React.PropsWithChildren<SvgProps>> = (props) => {
   return (
     <Svg viewBox="0 0 24 24" {...props}>
-      <image width="22" height="22" href="https://cdn-icons-png.flaticon.com/128/948/948057.png" alt="bridge" />
+      <image width="22" height="22" href="https://cdn-icons-png.flaticon.com/128/948/948057.png" />
     </Svg>
   );
 };
